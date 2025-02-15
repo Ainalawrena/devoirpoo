c90fc6fdefd8ecfe50e444cd22badd7ae86df90b
Ainalawrena <167605889+Ainalawrena@users.noreply.github.com>
2025-02-15 16:24:42 +0300

    Triangle.cpp

--- Forme_2d/Triangle.cpp
+++ Forme_2d/Triangle.cpp
@@ -17,10 +17,12 @@ void Triangle::setCote1(float valeur)
 {
     cote1 = valeur;
 }
+
 void Triangle::setCote2(float valeur)
 {
     cote2 = valeur;
 }
+
 void Triangle::setCote3(float valeur)
 {
     cote3 = valeur;
@@ -28,8 +30,18 @@ void Triangle::setCote3(float valeur)
 
 float Triangle::surface()
 {   
-    float hauteur = sqrt((cote3*cote3) - (cote1/2*cote1/2));
-    return (hauteur*cote1)/2; 
+    float hauteur(0);
+    if(cote1 == cote2 && cote2== cote3)
+    {
+        hauteur = sqrt((cote3*cote3) - (cote1/2*cote1/2));
+        return (hauteur*cote1)/2;
+    }
+    else
+    {
+        cout << "hauteur du triangle :" << "";
+        cin >> hauteur;
+        return (hauteur*cote1)/2;
+    }
 }
 
 float Triangle::perimetre()
@@ -38,7 +50,8 @@ float Triangle::perimetre()
 }
 
 void Triangle::affichage()
-{
-    cout << "Surface  du triangle : " << surface() << endl;
+{   
+    double s = surface();
+    cout << "Surface  du triangle : " << s << endl;
     cout << "Perimetre du triangle : " << perimetre() << endl;
 }
